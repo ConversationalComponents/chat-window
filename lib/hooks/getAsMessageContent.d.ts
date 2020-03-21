c21394a2d966f77625d7662ccf31fb78945dd82d
AlexTavor <alex@imperson.com>
2020-03-21 20:48:30 +0200

    A small update, bumped version to 1.5.1

---
+++ lib/hooks/getAsMessageContent.d.ts
@@ -0,0 +1,2 @@
+import { MessageContent } from "../../types";
+export declare const getAsMessageContent: (v: string | MessageContent[]) => MessageContent[];
--- src/lib/hooks/getAsMessageContent.tsx
+++ src/lib/hooks/getAsMessageContent.tsx
@@ -1,2 +1,3 @@
 import {MessageContent} from "../../types";
+
 export const getAsMessageContent = (v: MessageContent[] | string) => (typeof v === "string" ? [{text: v}] : v);
