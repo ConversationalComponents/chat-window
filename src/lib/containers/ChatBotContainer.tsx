6efdb1b3a27833c791599df740f20677c184e977
AlexTavor <alex@imperson.com>
2020-10-18 14:58:20 +0300

    Removed overflow:hidden from container

--- src/lib/containers/ChatBotContainer.tsx
+++ src/lib/containers/ChatBotContainer.tsx
@@ -1,29 +1,25 @@
 import React from "react";
 
-const ChatBotContainer = (p: { children: any }) => {
-
-
-  return (
-    <div
-      style={{
-        backgroundColor: "#f5f8fb",
-        borderRadius: "10px",
-        fontFamily: "sans-serif",
-        overflow: "hidden",
-        position: "relative",
-        zIndex: 999,
-        transform: "scale(1)",
-        transformOrigin: "bottom right",
-        transition: "transform 0.3s ease",
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        flexDirection: "column"
-      }}
-    >
-      {p.children}
-    </div>
-  );
+const ChatBotContainer = (p: {children: any}) => {
+    return (
+        <div
+            style={{
+                backgroundColor: "#f5f8fb",
+                borderRadius: "10px",
+                fontFamily: "sans-serif",
+                position: "relative",
+                zIndex: 999,
+                transform: "scale(1)",
+                transformOrigin: "bottom right",
+                transition: "transform 0.3s ease",
+                height: "100%",
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+            }}>
+            {p.children}
+        </div>
+    );
 };
 
 export default ChatBotContainer;
